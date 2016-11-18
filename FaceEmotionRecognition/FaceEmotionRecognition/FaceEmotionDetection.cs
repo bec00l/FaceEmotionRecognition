7eeb1d2b13ece11b17aacc11a7fa1608e9385fbe
David Hurd <imagitalestudios@gmail.com>
2016-11-17 22:02:25 -0500

    updated ui

---
+++ FaceEmotionRecognition/FaceEmotionRecognition/FaceEmotionDetection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using Microsoft.ProjectOxford.Face;
+using Plugin.Media;
+
+
+namespace FaceEmotionRecognition
+{
+    public class FaceEmotionDetection
+    {
+        public string Emotion { get; set; }
+        public double Smile { get; set; }
+        public string Glasses { get; set; }
+        public string Gender { get; set; }
+        public double Age { get; set; }
+        public double Beard { get; set; }
+        public double Moustache { get; set; }
+
+    }
+}
